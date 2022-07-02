972cec67eddc1a88079cd74e340bc47160085783
Alex Perathoner <alex.perathoner@gmail.com>
2022-07-02 22:26:45 +0200

    multiple playlists

--- script.py
+++ script.py
@@ -9,34 +9,35 @@ MAPBOX_TOKEN = os.environ.get('ITALIAANDREALORENZON_MAPBOX_TOKEN')
 YOUTUBE_TOKEN = os.environ.get('ITALIAANDREALORENZON_YOUTUBE_TOKEN')
 
 #https://python.tutorialink.com/extract-individual-links-from-a-single-youtube-playlist-link-using-python/
-url = 'https://www.youtube.com/playlist?list=PLokTFft4f9ScnHqKdGcIQFKlYGczbbUS9'
-query = parse_qs(urlparse(url).query, keep_blank_values=True)
-playlist_id = query["list"][0]
+urls = ['https://www.youtube.com/playlist?list=PLokTFft4f9ScnHqKdGcIQFKlYGczbbUS9', 'https://www.youtube.com/watch?v=hUOCOnynjWM&list=PLokTFft4f9SdmA43ku7l3fZf0JsIP_Il5', 'https://www.youtube.com/watch?v=cYSkKnAIZiQ&list=PLokTFft4f9SfAvfTR_4_NssXFPknqPYnZ']
+for url in urls
+    query = parse_qs(urlparse(url).query, keep_blank_values=True)
+    playlist_id = query["list"][0]
 
-youtube = googleapiclient.discovery.build("youtube", "v3", developerKey = YOUTUBE_TOKEN)
+    youtube = googleapiclient.discovery.build("youtube", "v3", developerKey = YOUTUBE_TOKEN)
 
-request = youtube.playlistItems().list(
-    part = "snippet",
-    playlistId = playlist_id,
-    maxResults = 50
-)
-response = request.execute()
-
-playlist_items = []
-while request is not None:
+    request = youtube.playlistItems().list(
+        part = "snippet",
+        playlistId = playlist_id,
+        maxResults = 50
+    )
     response = request.execute()
-    playlist_items += response["items"]
-    request = youtube.playlistItems().list_next(request, response)
 
-titles = []
-links = []
+    playlist_items = []
+    while request is not None:
+        response = request.execute()
+        playlist_items += response["items"]
+        request = youtube.playlistItems().list_next(request, response)
 
-print(f"total: {len(playlist_items)}")
-for t in playlist_items:
-    if(t["snippet"]["title"] != "Private video"):
-        #https://stackoverflow.com/a/60738277/6884062
-        titles.append(re.findall(r'\b[A-Z]+(?:\s+[A-Z]+)*\b', t["snippet"]["title"])[0])
-        links.append('https://www.youtube.com/embed/' + t["snippet"]["resourceId"]["videoId"])
+    titles = []
+    links = []
+
+    print(f"total: {len(playlist_items)}")
+    for t in playlist_items:
+        if(t["snippet"]["title"] != "Private video"):
+            #https://stackoverflow.com/a/60738277/6884062
+            titles.append(re.findall(r'\b[A-Z]+(?:\s+[A-Z]+)*\b', t["snippet"]["title"])[0])
+            links.append('https://www.youtube.com/embed/' + t["snippet"]["resourceId"]["videoId"])
 
 print(titles)
 print(links)
